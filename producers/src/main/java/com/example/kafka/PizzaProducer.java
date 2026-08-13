03494f26b998ccf40f9ab68da43e9a9e3c79afa5
icemokacat <icemokacat@gmail.com>
2026-08-13 10:16:25 +0900

    ✨ feat: 피자 메시지 생성 기능 추가
    새로운 피자 메시지를 생성하고 전송하는 기능이 추가되었습니다. 이 기능은 카프카를 통해 피자 관련 정보를 전송합니다.

---
+++ producers/src/main/java/com/example/kafka/PizzaProducer.java
@@ -0,0 +1,122 @@
+package com.example.kafka;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.javafaker.Faker;
+
+public class PizzaProducer {
+	private static final Logger log = LoggerFactory.getLogger(PizzaProducer.class);
+
+	public static void sendPizzaMessage(
+			KafkaProducer<String, String> kafkaProducer,
+			String topicName,
+			int iterCount,
+			int interIntervalMillis,
+			int intervalMillis,
+			int intervalCount,
+			boolean sync
+	)
+	{
+		PizzaMessage pizzaMessage = new PizzaMessage();
+		int iterSeq = 0;
+
+		long seed = 2022;
+		Random random = new Random(seed);
+		Faker faker = Faker.instance(random);
+
+		while ( iterSeq++ != iterCount){
+			HashMap<String, String> pMessage = pizzaMessage.produce_msg(faker,random, iterSeq);
+			ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName,
+				pMessage.get("key"), pMessage.get("message"));
+			// 메세지 전송
+			sendMessage(kafkaProducer, producerRecord, pMessage, sync);
+
+			if(intervalCount > 0 && (iterSeq % intervalCount == 0)){
+				try {
+					log.info("######### IntervalCount: {} intervalMillis: {} #############", intervalCount,
+						intervalMillis);
+					Thread.sleep(intervalMillis);
+				} catch (InterruptedException e) {
+					log.error(e.getMessage());
+				}
+			}
+
+			if(interIntervalMillis > 0){
+				try {
+					log.info("######### IntervalIntervalMillis: {} #############", interIntervalMillis);
+					Thread.sleep(intervalMillis);
+				} catch (InterruptedException e) {
+					log.error(e.getMessage());
+				}
+			}
+		} // end while
+	}
+
+	/**
+	 * sendMessage
+	 * */
+	public static void sendMessage(
+		KafkaProducer<String, String> kafkaProducer,
+		ProducerRecord<String, String> producerRecord,
+		HashMap<String, String> pMessage, boolean sync
+	)
+	{
+		if(!sync){
+			// 비동기 (async)
+			kafkaProducer.send(producerRecord,(metadata, exception) -> {
+				if(exception == null){
+					log.info("async(비동기) message:{} partition:{} offset:{}", pMessage.get("message"), metadata.partition(),
+						metadata.offset());
+				}else{
+					log.error("exception : {}", exception.getMessage());
+				}
+			});
+		}else{
+			try {
+				RecordMetadata metadata = kafkaProducer.send(producerRecord).get();
+				log.info("sync(동기) message:{} partition:{} offset:{}", pMessage.get("message"), metadata.partition(),
+					metadata.offset());
+			} catch (ExecutionException e) {
+				log.error("exception : {}", e.getMessage());
+			} catch (InterruptedException e) {
+				log.error("InterruptedException : {}", e.getMessage());
+			}
+		}// end if
+	}
+
+	public static   void  main(String[] args) {
+		String topicName = "pizza-topic";
+
+		Properties props = new Properties();
+
+		/*
+		 * broker set
+		 * - bootstrap server
+		 * - key.serializer.class
+		 * - value.serializer.class
+		 * */
+		props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.64.50:9092");
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		// KafkaProducer Object creation
+		//KafkaProducer<String, String> kafkaProducer;
+		KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+		try(kafkaProducer){
+			sendPizzaMessage(kafkaProducer, topicName, -1, 100, 1000, 100, true);
+		}
+
+	}
+
+}
